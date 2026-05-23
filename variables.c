ad63c33d8f46ab4d68caa11a29b18072c6c746f1
Farid4amiri <farid0amirizadeh@gmail.com>
2026-05-23 13:58:37 +0200

    adding double

--- variables.c
+++ variables.c
@@ -5,9 +5,13 @@ int main(){
  int age =25;
  int year = 2026;
  int quantity = 2;
+
  float gpa = 2.5;
  float price = 19.99;
  float temperature = -10.1;
+
+ double pi = 3.14159265358979;
+
 
     //prints
  printf("you are %d years old\n", age);
@@ -16,6 +20,7 @@ int main(){
  printf("your gpa is %.1f \n",gpa);
  printf("the price of item x is $%.2f \n", price);
  printf("the temperature is %.1f °C\n", temperature);
+ printf("the pi number is %f \n" , pi);
 
     return 0;
 }
