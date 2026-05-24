85ff5e2ae630841a757635547a38189a07e5a549
Farid4amiri <farid0amirizadeh@gmail.com>
2026-05-24 10:06:19 +0200

    finishing user input

--- user-input.c
+++ user-input.c
@@ -6,15 +6,26 @@ int main(){
     char grade = '\0';
     char name[30] = "";
 
-
+    //age
     printf("Enter your agae: ");
     scanf("%d", &age);
     printf("%d\n", age);
+
+    //gpa
     printf("Enter your GPA: ");
     scanf("%f",&gpa);
-    printf("%f",gpa);
+    printf("%.1f \n",gpa);
 
+    //grade
+    printf("Enter your grade: ");
+    scanf(" %c" , &grade);
+    printf("%c \n",grade);
 
+    //name
+    printf("Enter your name: ");
+    getchar(); //Clear input buffer
+    fgets(name,sizeof(name),stdin);
+    printf("%s \n",name);
 
     return 0;
 }
