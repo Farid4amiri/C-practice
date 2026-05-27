87ed4d6ac2ca22614b137a66c94ad54c23443b76
Farid4amiri <farid0amirizadeh@gmail.com>
2026-05-27 11:52:59 +0200

    pointer

--- function.c
+++ function.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int square(int num){
-  
+    
     return num * num;
 }
 
---
+++ pointers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main (){
+
+    int age = 25;
+    int *pAge = &age;
+    printf("%p\n", &age);
+    printf("%p\n", pAge);
+
+    
+    return 0;
+}
